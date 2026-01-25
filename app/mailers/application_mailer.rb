ff6707b0a641e8ef6079d37f3e67b251cf33cdd9
Angie Hemans <andrewhemans@gmail.com>
2026-01-25 15:01:13 -0800

    added email logo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: '"GoodSongs" <noreply@mg.goodsongs.app>'
   layout "mailer"
+  helper_method :api_url
 
   private
 
   def frontend_url
     ENV.fetch('FRONTEND_URL', 'https://goodsongs.app')
+  end
+
+  def api_url
+    ENV.fetch('API_URL', 'https://api.goodsongs.app')
   end
 end
